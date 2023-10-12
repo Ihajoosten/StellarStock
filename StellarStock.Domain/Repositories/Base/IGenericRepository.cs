ec52b9b943adfabcb34e73de1f9ce2becfa9ac90
Luc Joosten <lhajoosten@gmail.com>
2023-10-12 21:17:36 +0200

    Implemented IRepositories Domain layer

---
+++ StellarStock.Domain/Repositories/Base/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace StellarStock.Domain.Repositories.Base
+{
+    public interface IGenericRepository<T>
+    {
+        Task<T> GetByIdAsync(string id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<bool> AddAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> RemoveAsync(string id);
+    }
+}
---
+++ StellarStock.Domain/Repositories/IInventoryItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using StellarStock.Domain.Entities;
+using StellarStock.Domain.Repositories.Base;
+
+namespace StellarStock.Domain.Repositories
+{
+    public interface IInventoryItemRepository : IGenericRepository<InventoryItem>
+    {
+        Task<IEnumerable<InventoryItem>> GetByCategoryAsync(ItemCategory category);
+        Task<IEnumerable<InventoryItem>> GetLowStockItemsAsync(int threshold);
+        Task<IEnumerable<InventoryItem>> GetItemsExpiringSoonAsync(DateTime expirationDate);
+        Task<IEnumerable<InventoryItem>> SearchItemsAsync(string keyword);
+        Task<IEnumerable<InventoryItem>> GetItemsByPopularityScoreAsync(int minScore, int maxScore);
+        Task<IEnumerable<InventoryItem>> GetTopPopularItemsAsync(int count);
+        Task<IEnumerable<InventoryItem>> GetItemsInStockAsync();
+    }
+}
