21814a4b1138b4b2b0e02b56edac0ddc08c18230
Luc Joosten <lhajoosten@gmail.com>
2023-10-13 17:55:07 +0200

    Implemented Domain Services

---
+++ StellarStock.Domain/Services/Interfaces/IInventoryItemService.cs
@@ -0,0 +1,13 @@
+﻿using StellarStock.Domain.ValueObjects;
+
+namespace StellarStock.Domain.Services.Interfaces
+{
+    public interface IInventoryItemService
+    {
+        Task<bool> IsItemAvailableForSaleAsync(string itemId);
+        Task<bool> CanItemBeRemovedAsync(string itemId);
+        Task<bool> CanItemBeUpdatedAsync(string itemId, string newName, string newDescription, int newPopularityScore, QuantityVO newQuantity, MoneyVO newMoney);
+        Task<bool> CanItemBeMovedAsync(string itemId, string newLocationId);
+        Task<bool> CanItemBeExpiredAsync(string itemId);
+    }
+}
---
+++ StellarStock.Domain/Services/Interfaces/ISupplierService.cs
@@ -0,0 +1,12 @@
+﻿using StellarStock.Domain.ValueObjects;
+
+namespace StellarStock.Domain.Services.Interfaces
+{
+    public interface ISupplierService
+    {
+        Task<bool> CanSupplierBeUpdatedAsync(string supplierId, string newName, string newPhone, string newEmail, AddressVO newAddress);
+        Task<bool> CanSupplierBeDeletedAsync(string supplierId);
+        Task<bool> CanSupplierBeActivatedAsync(string supplierId);
+        Task<bool> CanSupplierBeDeactivatedAsync(string supplierId);
+    }
+}
